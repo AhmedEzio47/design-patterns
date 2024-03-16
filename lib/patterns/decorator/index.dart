335a9c36193645b9044152890e708838550c23b7
Ahmed Nabil <ahmednab93@gmail.com>
2024-03-16 21:24:41 +0200

    feat(decorator): add decorator pattern example

---
+++ lib/patterns/decorator/index.dart
@@ -0,0 +1,4 @@
+export 'base64_encoded_message.dart';
+export 'html_encoded_message.dart';
+export 'message.dart';
+export 'text_message.dart';
--- lib/patterns/index.dart
+++ lib/patterns/index.dart
@@ -3,5 +3,6 @@ export 'adapter/index.dart';
 export 'bridge/index.dart';
 export 'builder/index.dart';
 export 'chain_of_responsibility/index.dart';
+export 'decorator/index.dart';
 export 'factory/index.dart';
 export 'prototype/index.dart';
